51c52fed49fee03ad2d8cbb464a15f049bb45af9
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-01-29 16:46:35 +0100

    feat[intro]: add some animations and background, sounds need debugging

--- Assets/Scripts/SceneManagers/IntroManager.cs
+++ Assets/Scripts/SceneManagers/IntroManager.cs
@@ -24,12 +24,17 @@ public class IntroManager : MonoBehaviour
 
     public IEnumerator Scene()
     {
+        yield return new WaitForSeconds(0.1f);
         Darkener.GetComponent<Animation>().Play("Fade_in_1");
+        this.GetComponent<Animation>().Play("SoundFade_in");
         yield return new WaitForSeconds(6);
         FindFirstObjectByType<DialogueManager>().StartDialogue(dialogues[0]);
         yield return WaitUntilDialogueDone();
         yield return new WaitForSeconds(2f);
         FindFirstObjectByType<DialogueManager>().StartDialogue(dialogues[1]);
+        yield return WaitUntilDialogueDone();
+        yield return new WaitForSeconds(2f);
+        FindFirstObjectByType<DialogueManager>().StartDialogue(dialogues[2]);
         yield return WaitUntilDialogueDone();
         Darkener.GetComponent<Animation>().Play("Fade_in_2");
         DarkBackground.GetComponent<Animation>().Play("Background_disappears");
