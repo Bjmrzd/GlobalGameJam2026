dce38c2b709650014f5ed6c4ae0178e2f9aa676c
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-01-29 15:19:21 +0100

    feat[introScene]: first 10 secs of ga;eplay are done git status :DDDD

--- Assets/Scripts/SceneManagers/IntroManager.cs
+++ Assets/Scripts/SceneManagers/IntroManager.cs
@@ -6,9 +6,9 @@ using UnityEngine.UI;
 public class IntroManager : MonoBehaviour
 {
     public Dialogue[] dialogues;
-    public Image image;
-    public float FadeRate = 0.00001f;
-    private float targetAlpha;
+    public Image Darkener;
+    public Image Whitener;
+    public Image DarkBackground;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,12 +24,16 @@ public class IntroManager : MonoBehaviour
 
     public IEnumerator Scene()
     {
+        Darkener.GetComponent<Animation>().Play("Fade_in_1");
         yield return new WaitForSeconds(6);
         FindFirstObjectByType<DialogueManager>().StartDialogue(dialogues[0]);
         yield return WaitUntilDialogueDone();
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
         FindFirstObjectByType<DialogueManager>().StartDialogue(dialogues[1]);
         yield return WaitUntilDialogueDone();
+        Darkener.GetComponent<Animation>().Play("Fade_in_2");
+        DarkBackground.GetComponent<Animation>().Play("Background_disappears");
+        Whitener.GetComponent<Animation>().Play("Fade_in_white");
     }
 
     public IEnumerator WaitUntilDialogueDone()
