1f5655c56c38e056851af8c80efcfcc2e62f9c77
Benjamin Rouzaud <brouzaud-tmp@c2r8s5.marseille.42.school>
2026-01-29 07:21:05 +0100

    dialogue work

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,4 +5,8 @@ using System.Collections.Generic;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
 }
